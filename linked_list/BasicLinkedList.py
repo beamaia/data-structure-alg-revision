b6677c219b6b1c953a38f0d75685842d450c8f4a
Bea Maia <beamaia.work@gmail.com>
2022-05-14 16:14:43 -0300

    :sparkles: adds delete methods

--- linked_list/BasicLinkedList.py
+++ linked_list/BasicLinkedList.py
@@ -50,7 +50,7 @@ class BasicLinkedList:
         node.next = aux.next
         aux.next = node
     
-    def insertIndex(self, value_add, index) -> None:
+    def insertIndex(self, value_add, index:int) -> None:
         """
         Inserts node into a certain index of the list
         """
@@ -71,6 +71,63 @@ class BasicLinkedList:
         node.next = aux.next
         aux.next = node
 
+    def delete(self, value) -> None:
+        # If linked list is empty
+        if not self.head:
+            return 
+
+        # If value is head of linked list
+        if self.head.val == value:
+            self.head = self.head.next
+
+        # Searches for value in linked list
+        else:
+            # Searches head's next value and stores head's value
+            prev = self.head
+            aux = self.head.next
+            
+            # Searches value in list
+            while aux:
+                if aux.val == value:
+                    break
+                
+                prev = aux
+                aux = aux.next
+            
+
+            if not aux:
+                print(f'Value {value} not found')
+                return
+
+            prev.next = aux.next
+
+    def deleteIndex(self, index:int) -> None:
+        # If linked list is empty
+        if not self.head:
+            return 
+
+        # If index = 0
+        if not index:
+            self.head = self.head.next
+            return
+
+        aux = self.head.next
+        prev = self.head
+        counter = 1
+
+        while aux:
+            if counter == index:
+                break
+            prev = aux
+            aux = aux.next
+            counter += 1
+
+        if not aux:
+            print(f"Linked list doesn't have index {index}")
+            return 
+
+        prev.next = aux.next
+                
     def show_list(self) -> None:
         aux = self.head
         counter = 0
@@ -88,5 +145,6 @@ if __name__ == "__main__":
     linked_list.append(30)
     linked_list.insert(23, 2)
     linked_list.insertIndex(32, 1)
+    linked_list.deleteIndex(2)
 
     linked_list.show_list()
